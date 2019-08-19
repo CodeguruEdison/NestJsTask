495c5e085dfddd324d20143bf19ff598dca39c3e
Ayaskanta Samal <silu.samal@gmail.com>
2019-08-18 22:25:56 -0400

    first commit on postgress

--- task-management/src/tasks/dto/get-task-filter.dto.ts
+++ task-management/src/tasks/dto/get-task-filter.dto.ts
@@ -2,7 +2,7 @@ import { TaskStatus } from '../task.model';
 import { IsOptional, IsIn, IsNotEmpty } from 'class-validator';
 
 export class GetTaskFilterDto {
-    @IsOptional()
+    @IsOptional() 
     @IsIn([TaskStatus.DONE, TaskStatus.IN_PROGRESS, TaskStatus.OPEN])
     status: TaskStatus;
     @IsOptional()
